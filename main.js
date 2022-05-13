914fd4a78561a7b8e9472df38f62e7424c16a003
Emilio Pino <pinoen@yahoo.com.ar>
2022-05-13 16:38:28 -0300

    Add functionality so appear one blank space for each letter

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 let palabras = ['ESPADA', 'HOGAR', 'COMPUTADORA', 'CELULAR', 'MESA', 'COLCHON', 'MUEBLE', 'PLATO', 'COMIDA', 'TIERRA', 'TELEVISOR', 'PLANCHA', 'QUESO', 'HAMBURGUESA', 'CALLE', 'AVENIDA', 'CUADERNO', 'TERMOTANQUE', 'BOTELLA', 'SILLA', 'HELADERA', 'ELEFANTE', 'HORMIGA', 'ZAPATILLA', 'ANCIANO', 'MOCHILA', 'PIZZA', ' AUTOMOVIL', 'ACEITE', 'PILETA', 'ALMOHADA'];
 
 let palabra = palabras[Math.floor(Math.random()*palabras.length)];
-console.log(palabra);
+let numeroLetras = palabra.length;
 
+let palabraSecreta = document.querySelector('.letras');
+let espacio = document.querySelector('#secreto');
+let letra = document.createElement('span');
 
 let empezar = document.querySelector('#empezar');
 let nuevaPalabra = document.querySelector('#nueva-palabra');
 let horca = document.querySelector('#horca');
-let cabeza = document.querySelector('#prisoner-head');
 
 empezar.addEventListener('click', ()=>{
   empezar.style.display = 'none';
   nuevaPalabra.style.display = 'none';
   horca.style.display = 'flex';
 
+  console.log(palabra);
+
+  let espaciosLetras = '';
+
+  for(let i=0; i<palabra.length; i++){
+    espaciosLetras += ' _ ';
+  }
+
+  palabraSecreta.textContent = espaciosLetras;
+
+  
 
 
 })
